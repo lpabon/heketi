e97972846cb90511160cfb9c7525d9af29ddbd28
Luis Pabón <lpabon@redhat.com>
2016-06-19 22:42:39 -0400

    Installs OpenShift Origin 1.2.0

--- tests/functional/TestOpenShiftSmokeTest/vagrant/up.sh
+++ tests/functional/TestOpenShiftSmokeTest/vagrant/up.sh
@@ -2,3 +2,4 @@
 
 vagrant up --no-provision $@
 vagrant provision
+vagrant ssh client --command "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.conf openshift-ansible/playbooks/byo/config.yml"
